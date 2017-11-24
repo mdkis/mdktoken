949a7a81c2ac7ac41b0a890e84a01b793aa77e0f
Tim <truecarry@gmail.com>
2017-11-24 16:11:48 +0300

    Use token vestion from OZ

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 var MDKToken = artifacts.require("./MDKToken.sol");
+var MDKPreICO = artifacts.require("./MDKPreICO.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MDKToken);
+  deployer.deploy(MDKToken).then(() => {
+    deployer.deploy(MDKPreICO, (Date.now() / 1000) + 120, //Start 2 minutes after
+      (Date.now() / 1000) + (60 * 60 * 24 * 31), //End 1 month after
+      22500,
+      MDKToken.address);
+  })
 };
