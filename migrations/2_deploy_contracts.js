68d4e9b64138bc3e4b3c96f256ee1994c4f005db
Tim <truecarry@gmail.com>
2017-11-24 16:18:19 +0300

    Don't deplot PreICO in dev network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var MDKToken = artifacts.require("./MDKToken.sol");
 var MDKPreICO = artifacts.require("./MDKPreICO.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(MDKToken).then(() => {
+module.exports = function(deployer, network) {
+  deployer.deploy(MDKToken)
+  .then(() => {
+    if (network !== 'rinkeby' && network !== 'main') return
     deployer.deploy(MDKPreICO, (Date.now() / 1000) + 120, //Start 2 minutes after
       (Date.now() / 1000) + (60 * 60 * 24 * 31), //End 1 month after
       22500,
